4f13dd63a28800580be6ca4373fc55eb0518d94d
Thomas Langlois <thomas.langlois@mac.com>
2021-09-24 21:19:50 +0000

    tw

--- lobster-app/src/App.js
+++ lobster-app/src/App.js
@@ -37,8 +37,8 @@ function App() {
                       </div>} */}
                       <div className="flex justify-start space-x-2">
                       <p className="max-w-sm text-lg md:text-2xl">Thank you all for voting, my name is:</p>
-                      <h2 className="text-2xl md:text-3xl font-semibold text-red-600">THOMAS</h2>
                       </div>
+                      <h1 className="text-2xl md:text-3xl font-semibold text-red-600">THOMAS</h1>
                     </React.Fragment>
                 }>
                 </NftStatus>
