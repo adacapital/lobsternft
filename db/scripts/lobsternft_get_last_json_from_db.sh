5b72ee4cb7e9c7302755ec5103eca026a769180f
Thomas Langlois <thomas.langlois@mac.com>
2021-09-25 00:30:10 +0000

    tw

--- db/scripts/lobsternft_get_last_json_from_db.sh
+++ db/scripts/lobsternft_get_last_json_from_db.sh
@@ -11,15 +11,37 @@ TIME=$(echo $LAST | awk -F"|" '{print $2}')
 LOBSTER_COUNTER=$(echo $LAST | awk -F"|" '{print $3}')
 LOBSTER_VOTES=$(echo $LAST | awk -F"|" '{print $4}')
 
-LOBSTER_COUNTER=24824
-LOBSTER_VOTES=501
+# LOBSTER_COUNTER=24824
+# LOBSTER_VOTES=501
 
-if [[ $LOBSTER_VOTES > 500 ]]; then
-    NAME_INDEX=$(expr $LOBSTER_COUNTER % 1219)
-    NAME_INDEX=$(expr $NAME_INDEX + 1)
-    NAME=$(tail +$NAME_INDEX $SCRIPT_DIR/names.txt | head -1 | awk '{print $2}')
+# if [[ $LOBSTER_VOTES > 500 ]]; then
+#     NAME_INDEX=$(expr $LOBSTER_COUNTER % 1219)
+#     NAME_INDEX=$(expr $NAME_INDEX + 1)
+#     NAME=$(tail +$NAME_INDEX $SCRIPT_DIR/names.txt | head -1 | awk '{print $2}')
 
-    $(cat <<-END > ./tmp.json
+#     $(cat <<-END > ./tmp.json
+# {
+#     "id": "${ID}", 
+#     "time": "${TIME}", 
+#     "lobster_counter": ${LOBSTER_COUNTER},
+#     "lobster_votes": ${LOBSTER_VOTES},
+#     "name": "${NAME}"
+# }
+# END
+# )
+# else
+#     $(cat <<-END > ./tmp.json
+# {
+#     "id": "${ID}", 
+#     "time": "${TIME}", 
+#     "lobster_counter": ${LOBSTER_COUNTER},
+#     "lobster_votes": ${LOBSTER_VOTES}
+# }
+# END
+# )
+# fi
+
+$(cat <<-END > ./tmp.json
 {
     "id": "${ID}", 
     "time": "${TIME}", 
@@ -29,17 +51,6 @@ if [[ $LOBSTER_VOTES > 500 ]]; then
 }
 END
 )
-else
-    $(cat <<-END > ./tmp.json
-{
-    "id": "${ID}", 
-    "time": "${TIME}", 
-    "lobster_counter": ${LOBSTER_COUNTER},
-    "lobster_votes": ${LOBSTER_VOTES}
-}
-END
-)
-fi
 
 cat ./tmp.json
 rm -f ./tmp.json
