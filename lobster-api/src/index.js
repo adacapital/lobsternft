60180019b231c29511893ae1e300970d01cdcf3d
Thomas Langlois <thomas.langlois@mac.com>
2021-09-25 00:36:20 +0000

    tw

--- lobster-api/src/index.js
+++ lobster-api/src/index.js
@@ -43,6 +43,7 @@ app.get("/voting-status", (req,res,next)=> {
             res.status(404).json({err: "no data available"});
         } else {
             const row = rows[0];
+            console.log("row" + row);
             res.status(200).json({progressPct: 100*(row.lobster_votes / NFT_VOTES_TARGET), ratioDisplay: `${row.lobster_votes}/${NFT_VOTES_TARGET}`, name: `${row.name}`});
         }
     })
